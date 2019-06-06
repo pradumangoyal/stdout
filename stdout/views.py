54f5b25385ddc7f3f7b5da3aa51baed0aab5c2c8
Praduman Goyal <goyal.praduman611@gmail.com>
2019-06-06 18:46:07 +0530

    Initialise and finish

---
+++ stdout/views.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        if 'stdout' in request.path:
+            subprocess.run(["lpr", settings.BASE_DIR+uploaded_file_url])
+        return render(request, 'stdout.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'stdout.html')
